d99788130c37ca95c521ae7ded4c24bc562f9495
tadessedev <tadessedev@gmail.com>
2024-07-29 18:36:16 +0300

    update client-server-connection

--- client.js
+++ client.js
@@ -9,3 +9,8 @@ const netClient = new net.createConnection(3000, () => {
 netClient.on("connect", () => {
   console.log("connect");
 });
+
+netClient.on("data", (data) => {
+  console.log("data", data.toString());
+  netClient.write("data from client");
+});
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const net = require("net");
 
 const netServer = new net.Server((socket) => {
   console.log("server created with socket", socket.address());
+  socket.write("hello from server");
 });
 
 netServer.listen(3000);
 netServer.on("listening", () => {
   console.log("listening");
 });
+
