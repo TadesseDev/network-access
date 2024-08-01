f20e00f1bf0998a22d1829560b07facb5176de94
tadessedev <tadessedev@gmail.com>
2024-08-01 19:08:39 +0300

    update multiple client

--- server.js
+++ server.js
@@ -2,9 +2,17 @@
 
 const net = require("net");
 
+// function main() {
+const peerSockets = new Map();
 const netServer = new net.Server((socket) => {
-  console.log("server created with socket", socket.address());
-  socket.write("hello from server");
+  console.log("server created with socket", socket._sockname);
+  peerSockets.set(new Date().getTime(), {
+    ip: socket.localAddress,
+    family: socket.localFamily,
+    port: socket.localPort,
+  });
+  console.log("peerSockets", peerSockets);
+  socket.write(JSON.stringify(Object.fromEntries(peerSockets)));
 });
 
 netServer.listen(3000);
@@ -12,3 +20,9 @@ netServer.on("listening", () => {
   console.log("listening");
 });
 
+// process.nextTick(() => {
+//   console.info("server started");
+// });
+// }
+
+// main();
